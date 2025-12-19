d8ebc0e1defe9e2b7288a3d603b356f03726f610
RLASH18 <lacdangryan18@gmail.com>
2025-12-19 23:34:08 +0800

    fix: update PostFactory to use paragraph for description generation

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,7 +18,7 @@ class PostFactory extends Factory
     {
         return [
             'title' => fake()->title(6),
-            'description' => fake()->text(2),
+            'description' => fake()->paragraph(2),
         ];
     }
 }
